01ede530fe41c3945cf0dfb1d917749bb7c9a9bb
Vincent <maron.vincent@outlook.fr>
2022-01-27 23:35:08 +0100

    fix, display

--- 1/main.jl
+++ 1/main.jl
@@ -3,12 +3,13 @@ using JuMP
 
 model = Model(CPLEX.Optimizer)
 
-include("ReserveNaturelle.dat")
+io = open("temp", "w")
+write(io, replace(read(open("ReserveNaturelles_opl.dat"), String), ";"=>""))
+close(io)
+include("temp")
+rm("temp")
 
-#N = 10
 K = p + q
-#replace(read(open("ReserveNaturelles_opl.dat"), String), ";"=>"")
-
 rare = [species <= p for species in 1:K]
 
 
@@ -29,18 +30,27 @@ survival_probabilities = @constraint(model,[k in 1:K],
 
 @constraint(model, [i in 1:n, j in 1:m, (k,l) in neighbours(i,j)], y[i,j] <= x[k,l])
 
-# @constraint(model,[j in 1:m], y[0,j]==0)
-# @constraint(model,[j in 1:m], y[n,j]==0)
-# @constraint(model,[i in 1:n], y[i,0]==0)
-# @constraint(model,[i in 1:n], y[i,m]==0)
+@constraint(model,[j in 1:m], y[1,j]==0)
+@constraint(model,[j in 1:m], y[n,j]==0)
+@constraint(model,[i in 1:n], y[i,1]==0)
+@constraint(model,[i in 1:n], y[i,m]==0)
 
 @objective(model, Min, sum(c[i][j]*x[i,j] for i in 1:n, j in 1:m))
 
 optimize!(model)
 if has_values(model)
     print(solution_summary(model, verbose=true))
-    for survival_probability in survival_probabilities
-        println("Survival probabilities: ---> ", value(survival_probability) ," ", name(survival_probability))
+    for i in 1:n
+        for j in 1:m
+            if value.(y[i, j])==1
+                print("O")
+            elseif value.(x[i, j])==1
+                print("o")
+            else
+                print("_")
+            end
+        end
+        println()
     end
 else
     print("No solution")
